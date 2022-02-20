0c0fbd3482596821103c5150b221099e0f57cd60
ElementSi <eg.zzz.747@gmail.com>
2022-02-20 11:48:05 +0300

    Have done MyPic2

--- Sem2/lab3_Drawing/MyPic1.py
+++ Sem2/lab3_Drawing/MyPic1.py
@@ -4,7 +4,7 @@ import pygame.draw as dr
 pygame.init()
 
 FPS = 30
-screen = pygame.display.set_mode((600, 800))
+Screen = pygame.display.set_mode((600, 800))
 
 # Creating palette
 Black = (0, 0, 0)
@@ -24,72 +24,72 @@ WindowYellow = (240, 196, 53)
 GhostBlue = (108, 211, 240)
 
 # Drawing background
-screen.fill(Black)
-dr.rect(screen, SkyGray, (0, 0, 600, 330))
-dr.circle(screen, MoonGray, (540, 70), 50, 0)
-dr.ellipse(screen, Cloud3Gray, (300, 200, 500, 50), 0)
+Screen.fill(Black)
+dr.rect(Screen, SkyGray, (0, 0, 600, 330))
+dr.circle(Screen, MoonGray, (540, 70), 50, 0)
+dr.ellipse(Screen, Cloud3Gray, (300, 200, 500, 50), 0)
 
 # Drawing building
-dr.rect(screen, BuildingBrown, (40, 175, 310, 400))
-dr.polygon(screen, RoofGray, [(5, 175), (50, 135), (320, 135), (385, 175)], 0)
+dr.rect(Screen, BuildingBrown, (40, 175, 310, 400))
+dr.polygon(Screen, RoofGray, [(5, 175), (50, 135), (320, 135), (385, 175)], 0)
 
 # Drawing clouds and chimneys
-dr.rect(screen, ChimneyGray, (85, 105, 10, 50))
-dr.rect(screen, ChimneyGray, (225, 85, 8, 50))
-dr.ellipse(screen, Cloud2Gray, (25, 65, 450, 60), 0)
-dr.ellipse(screen, Cloud1Gray, (260, 35, 350, 60), 0)
-dr.ellipse(screen, Cloud1Gray, (370, 115, 450, 45), 0)
-dr.rect(screen, ChimneyGray, (100, 85, 18, 75))
-dr.rect(screen, ChimneyGray, (295, 100, 11, 60))
+dr.rect(Screen, ChimneyGray, (85, 105, 10, 50))
+dr.rect(Screen, ChimneyGray, (225, 85, 8, 50))
+dr.ellipse(Screen, Cloud2Gray, (25, 65, 450, 60), 0)
+dr.ellipse(Screen, Cloud1Gray, (260, 35, 350, 60), 0)
+dr.ellipse(Screen, Cloud1Gray, (370, 115, 450, 45), 0)
+dr.rect(Screen, ChimneyGray, (100, 85, 18, 75))
+dr.rect(Screen, ChimneyGray, (295, 100, 11, 60))
 
 # Drawing building details
-dr.rect(screen, BuildingBeige, (60, 176, 35, 150))
-dr.rect(screen, BuildingBeige, (120, 176, 35, 150))
-dr.rect(screen, BuildingBeige, (200, 176, 35, 150))
-dr.rect(screen, BuildingBeige, (280, 176, 35, 150))
+dr.rect(Screen, BuildingBeige, (60, 176, 35, 150))
+dr.rect(Screen, BuildingBeige, (120, 176, 35, 150))
+dr.rect(Screen, BuildingBeige, (200, 176, 35, 150))
+dr.rect(Screen, BuildingBeige, (280, 176, 35, 150))
 
 # Drawing balcony
-dr.rect(screen, BalconyGray, (5, 340, 380, 40))
+dr.rect(Screen, BalconyGray, (5, 340, 380, 40))
 for i in range(10, 380, 60):
-    dr.rect(screen, BalconyGray, (i, 300, 10, 40))
-dr.rect(screen, BalconyGray, (20, 280, 350, 20))
+    dr.rect(Screen, BalconyGray, (i, 300, 10, 40))
+dr.rect(Screen, BalconyGray, (20, 280, 350, 20))
 
 # Drawing windows
-dr.rect(screen, WindowBrown, (80, 460, 60, 70))
-dr.rect(screen, WindowBrown, (165, 460, 60, 70))
-dr.rect(screen, WindowYellow, (250, 460, 60, 70))
+dr.rect(Screen, WindowBrown, (80, 460, 60, 70))
+dr.rect(Screen, WindowBrown, (165, 460, 60, 70))
+dr.rect(Screen, WindowYellow, (250, 460, 60, 70))
 
 # Drawing ghost body
-dr.circle(screen, GhostGray, (475, 600), 25, 0)
-dr.circle(screen, GhostGray, (410, 700), 5, 0)
-dr.circle(screen, GhostGray, (470, 710), 18, 0)
-dr.circle(screen, GhostGray, (530, 690), 30, 0)
-dr.circle(screen, GhostGray, (565, 650), 20, 0)
-dr.circle(screen, GhostGray, (500, 615), 20, 0)
-dr.polygon(screen, GhostGray,
+dr.circle(Screen, GhostGray, (475, 600), 25, 0)
+dr.circle(Screen, GhostGray, (410, 700), 5, 0)
+dr.circle(Screen, GhostGray, (470, 710), 18, 0)
+dr.circle(Screen, GhostGray, (530, 690), 30, 0)
+dr.circle(Screen, GhostGray, (565, 650), 20, 0)
+dr.circle(Screen, GhostGray, (500, 615), 20, 0)
+dr.polygon(Screen, GhostGray,
            [(450, 600), (447, 630), (427, 654), (420, 682), (406, 698), (414, 702), (436, 694), (455, 705),
             (455, 700), (485, 716), (520, 700), (550, 680), (566, 664), (574, 641), (512, 600), (450, 600)], 0)
-dr.circle(screen, Black, (400, 612), 51, 0)
-dr.circle(screen, Black, (400, 650), 30, 0)
-dr.circle(screen, Black, (443, 710), 17, 0)
-dr.circle(screen, Black, (445, 725), 17, 0)
-dr.circle(screen, Black, (445, 725), 17, 0)
-dr.circle(screen, Black, (502, 714), 17, 0)
-dr.circle(screen, Black, (565, 680), 14, 0)
-dr.circle(screen, Black, (520, 745), 35, 0)
-dr.circle(screen, Black, (573, 563), 70, 0)
-dr.circle(screen, GhostGray, (509, 674), 36, 0)
-dr.circle(screen, GhostGray, (484, 698), 20, 0)
-dr.circle(screen, GhostGray, (521, 645), 35, 0)
-dr.circle(screen, Black, (574, 695), 21, 0)
+dr.circle(Screen, Black, (400, 612), 51, 0)
+dr.circle(Screen, Black, (400, 650), 30, 0)
+dr.circle(Screen, Black, (443, 710), 17, 0)
+dr.circle(Screen, Black, (445, 725), 17, 0)
+dr.circle(Screen, Black, (502, 714), 17, 0)
+dr.circle(Screen, Black, (565, 680), 14, 0)
+dr.circle(Screen, Black, (520, 745), 35, 0)
+dr.circle(Screen, GhostGray, (509, 674), 36, 0)
+dr.circle(Screen, GhostGray, (484, 698), 20, 0)
+dr.circle(Screen, GhostGray, (521, 645), 35, 0)
+dr.circle(Screen, Black, (574, 695), 21, 0)
+dr.circle(Screen, Black, (542, 587), 32, 0)
+dr.circle(Screen, Black, (555, 597), 32, 0)
 
 # Drawing ghost eyes
-dr.circle(screen, GhostBlue, (464, 596), 6, 0)
-dr.circle(screen, GhostBlue, (484, 592), 6, 0)
-dr.circle(screen, Black, (462, 596), 2, 0)
-dr.circle(screen, Black, (482, 592), 2, 0)
-dr.polygon(screen, MoonGray, [(463, 595), (468, 592), (469, 594), (466, 595), (463, 595)], 0)
-dr.polygon(screen, MoonGray, [(483, 591), (488, 588), (488, 590), (488, 591), (483, 591)], 0)
+dr.circle(Screen, GhostBlue, (464, 596), 6, 0)
+dr.circle(Screen, GhostBlue, (484, 592), 6, 0)
+dr.circle(Screen, Black, (462, 596), 2, 0)
+dr.circle(Screen, Black, (482, 592), 2, 0)
+dr.polygon(Screen, MoonGray, [(463, 595), (468, 592), (469, 594), (466, 595), (463, 595)], 0)
+dr.polygon(Screen, MoonGray, [(483, 591), (488, 588), (488, 590), (488, 591), (483, 591)], 0)
 
 pygame.display.update()
 clock = pygame.time.Clock()
